049e31dd42e8ac9f80e2dd53c760d40f3be9eb6b
César Barone <cesar.bmcosta@gmail.com>
2015-11-04 09:14:51 -0200

    support for panh

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+images_directory="$1"
+cd "$images_directory"
+
 for file in *.jpg
 do
-    convert  -resize 640X480  "$file"  small_"$file"
+  echo converting "$file" ...
+  convert  -resize 640X480  "$file" "$file"
 done
+
+cd -
