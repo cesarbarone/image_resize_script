2959ae9124fb700aef7387b9280df15032e1a020
César Barone <cesar.bmcosta@gmail.com>
2016-01-08 16:54:20 -0200

    jpg and JPG support

--- script.rb
+++ script.rb
@@ -1,4 +1,4 @@
-Dir.glob('../**/*.jpg').each do |image|
+Dir.glob(['../**/*.jpg', '../**/*.JPG' ]).each do |image|
   puts "converting #{image}..."
   `convert  -resize 640X480  #{image} #{image}`
 end
