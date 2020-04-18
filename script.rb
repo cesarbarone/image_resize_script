99155a2c5964bddd18fd4178b730550f3aaacfd2
César Barone <cesar.bmcosta@gmail.com>
2020-04-18 09:26:23 -0300

    auto-update

--- script.rb
+++ script.rb
@@ -1,4 +1,18 @@
-Dir.glob(['*.jpg', '*.JPG' ]).each do |image|
-  puts "converting #{image}..."
-  `convert  -resize 640X480  #{image} #{image}`
+
+def resize
+  Dir.glob(['*.jpg', '*.JPG' ]).each do |image|
+    puts "converting #{image}..."
+    `convert  -resize 640X480  #{image} #{image}`
+  end
 end
+
+def merge
+  Dir.glob(['*.jpg', '*.JPG' ]).each do |image|
+    puts "merging #{image}..."
+    `convert  bg.jpg #{image}  -gravity center -composite -compose over #{image}`
+    `convert -rotate 90 #{image} #{image}`
+  end
+end
+
+
+merge
